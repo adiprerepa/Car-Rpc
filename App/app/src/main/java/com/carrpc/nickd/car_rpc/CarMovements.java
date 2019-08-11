8e85ef9c0e227a21746557e6636ed0236079c667
Aditya Prerepa <adiprerepa@gmail.com>
2019-08-11 11:17:57 -0700

    .travis.yml initial

--- App/app/src/main/java/com/carrpc/nickd/car_rpc/CarMovements.java
+++ App/app/src/main/java/com/carrpc/nickd/car_rpc/CarMovements.java
@@ -4,19 +4,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.*;
-import android.widget.TextView;
-
 
 public class CarMovements extends AppCompatActivity {
-        ImageButton left = (ImageButton) findViewById(R.id.leftArr);
-        ImageButton right = (ImageButton) findViewById(R.id.rightArr);
-        ImageButton up = (ImageButton) findViewById(R.id.upArr);
-        ImageButton down = (ImageButton) findViewById(R.id.downArr);
+    ImageButton left = findViewById(R.id.leftArr);
+    ImageButton right = findViewById(R.id.rightArr);
+    ImageButton up = findViewById(R.id.upArr);
+    ImageButton down = findViewById(R.id.downArr);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_car_movements);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_car_movements);
         left.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
