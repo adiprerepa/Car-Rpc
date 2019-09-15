517e99296d85f75c675f6382756245df5e9e006b
Aditya Prerepa <adiprerepa@gmail.com>
2019-09-14 18:25:57 -0700

    Test code written; still need to remove 3rd hcsr04 from code and fill in motor pins (#24)

--- Esp8266/Esp8266.ino
+++ Esp8266/Esp8266.ino
@@ -1,4 +1,4 @@
-   qq#include "pb_common.h"
+#include "pb_common.h"
 #include "pb.h"
 #include "pb_encode.h"
 #include "pb_decode.h"
@@ -6,38 +6,52 @@
 
 #include <ESP8266WiFi.h>
 
-const char* ssid     = "SSID";
-const char* pass     = "PASS";
+const char* ssid     = "NETGEAR11";
+const char* pass     = "huskyjet126";
 const uint16_t port  = 42069;
+
+// remove 1 element from the following lists to make GPIO room for the motors
+
+const int trigPins[3] = { 14, 13, 5 };
+const int echoPins[3] = { 12, 15, 4 };
+
+int forwardPin = ;
+int backwardPin = ;
+int leftPin = ;
+int rightPin = ;
+
+// remove 1 element from the following list to make GPIO room for the motors
+
+int distances[3] = { 0, 0, 0 };
 
 WiFiServer server(port);
 WiFiClient client;
 
 struct InputData {
-  static const unsigned buffer_length = esp8266_Esp8266_Command_size;
+  static const unsigned buffer_length = Esp8266_Full_Request_size;
   unsigned char buffer[buffer_length];
   pb_istream_t input_stream;
-  esp8266_Esp8266_Command command;
+  Esp8266_Full_Request full_request;
 
   InputData() { refresh(); };
 
   void refresh() {
     input_stream = pb_istream_from_buffer(buffer, buffer_length);
-    command = esp8266_Esp8266_Command_init_zero;
+    full_request = Esp8266_Full_Request_init_zero;
   }
 
   void readAndDecode() {
     refresh();
     client.read(buffer, client.available());
-    pb_decode(&input_stream, esp8266_Esp8266_Command_fields, &command);
+    pb_decode(&input_stream, Esp8266_Full_Request_fields, &full_request);
   }
 } input;
 
 struct OutputData {
-  static const unsigned buffer_length = esp8266_Esp8266_Metrics_size;
+  static const unsigned buffer_length = Esp8266_Metrics_size;
   unsigned char buffer[buffer_length];
   pb_ostream_t output_stream;
-  esp8266_Esp8266_Metrics metrics;
+  Esp8266_Metrics metrics;
 
   OutputData() { refresh(); }
   void refresh() {
@@ -46,13 +60,16 @@ struct OutputData {
 
   void encodeAndWrite() {
     refresh();
-    pb_encode(&output_stream, esp8266_Esp8266_Metrics_fields, &metrics);
+    pb_encode(&output_stream, Esp8266_Metrics_fields, &metrics);
     client.write(buffer, output_stream.bytes_written);
   }
 } output;
 
 void setupWiFi();
+void setupPins();
+
 void printConnection(const char * name, IPAddress address, int port);
+void getDistance();
 
 void handleData();
 
@@ -60,22 +77,28 @@ void handleData();
 void setup() {
   Serial.begin(9600);
   setupWiFi();
+  setupPins();
 }
 
 
 void loop() {
-  client = server.available();
-  if (client) {
-    printConnection("controller", client.remoteIP(), client.remotePort());
-    client.setNoDelay(true);
-    while (client.connected()) {
-      if (client.available()) {
-      input.readAndDecode();
-      handleData();
-      output.encodeAndWrite();
-      }
-    }
-    Serial.println("Disconnected");
+  //client = server.available();
+  //if (client) {
+  //  printConnection("controller", client.remoteIP(), client.remotePort());
+  //  client.setNoDelay(true);
+  //  while (client.connected()) {
+  //    if (client.available()) {
+  //    input.readAndDecode();
+  //    handleData();
+  //    output.encodeAndWrite();
+  //    }
+  //  }
+  //  Serial.println("Disconnected");
+  //}
+  while (true) {
+  Serial.print('b');
+    getDistance();
+    delay(100);
   }
 }
 
@@ -93,6 +116,22 @@ void setupWiFi() {
   printConnection("WiFi", WiFi.localIP(), port);
 }
 
+void setupPins() {
+  pinMode(trigPins[0], OUTPUT);
+  pinMode(echoPins[0], INPUT);
+  pinMode(trigPins[1], OUTPUT);
+  pinMode(echoPins[1], INPUT);
+
+  pinMode(trigPins[2], OUTPUT); // remove this hcsr04 to make space for motors
+  pinMode(echoPins[2], INPUT);
+
+  pinMode(forwardPin, OUTPUT);
+  pinMode(backwardPin, OUTPUT);
+  pinMode(leftPin, OUTPUT);
+  pinMode(rightPin, OUTPUT);
+}
+
+
 void printConnection(const char * name, IPAddress address, int port) {
   Serial.print("Connected to ");
   Serial.print(name);
@@ -103,14 +142,66 @@ void printConnection(const char * name, IPAddress address, int port) {
 }
 
 void handleData() {
-  Serial.print("got: ");
-  switch (input.command.direction) {
-    case esp8266_Esp8266_Command_Esp8266_Direction_MOTOR_FORWARD: Serial.println("MOTOR FORWARD"); break;
-    case esp8266_Esp8266_Command_Esp8266_Direction_MOTOR_BACK: Serial.println("MOTOR BACK"); break;
-    case esp8266_Esp8266_Command_Esp8266_Direction_WHEELS_LEFT: Serial.println("WHEELS LEFT"); break;
-    case esp8266_Esp8266_Command_Esp8266_Direction_WHEELS_RIGHT: Serial.println("WHEELS RIGHT"); break;
-    case esp8266_Esp8266_Command_Esp8266_Direction_KEEP_ALIVE: Serial.println("KEEP ALIVE"); break;
+  Serial.print("Front Rotation: ");
+  Serial.println(input.full_request.frontRotation);
+  Serial.print("Speed: ");
+  Serial.println(input.full_request.speed);
+
+  // handle motors
+  if (full_request.command.front_rotation == 0) {
+      digitalWrite(leftPin, LOW);
+      digitalWrite(rightPin, LOW);
+  } else  if (full_request.command.front_rotation < 0) {
+      digitalWrite(leftPin, HIGH);
+      digitalWrite(rightPin, LOW);
+  } else if (full_request.command.front_rotation > 0) {
+      digitalWrite(leftPin, LOW);
+      digitalWrite(rightPin, HIGH);
   }
-  output.metrics.hcsr04_calibrate = 1.0;
-  output.metrics.returnCode = esp8266_Esp8266_Metrics_Esp8266_Return_Status_Codes_OPERATION_OK;
+
+  if (full_request.command.speed == 0) {
+      digitalWrite(backwardPin, LOW);
+      digitalWrite(forwardPin, LOW);
+  } else  if (full_request.command.speed < 0) {
+      digitalWrite(backwardPin, HIGH);
+      digitalWrite(forwardPin, LOW);
+  } else if (full_request.command.speed > 0) {
+      digitalWrite(backwardPin, LOW);
+      digitalWrite(forwardPin, HIGH);
+  }
+
+
+  getDistance();
+
+  // remove 1 of the following lines to use 2 hcsrs to make space for the motors (you will also have to change the proto)
+  output.metrics.HCSR04_front_distance = echoPins[0];
+  output.metrics.HCSR04_left_distance = echoPins[1];
+  output.metrics.HCSR04_right_distance = echoPins[2];
+}
+
+long duration;
+int distance;
+
+
+void getDistance() {
+  // remove one iteration of this loop to remove an hcsr04 to make space for motors
+  for (int i = 0; i < 3; i++) {
+    digitalWrite(trigPins[i], LOW);
+    delayMicroseconds(2);
+
+    // Sets the trigPin on HIGH state for 10 micro seconds
+    digitalWrite(trigPins[i], HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPins[i], LOW);
+
+    // Reads the echoPin, returns the sound wave travel time in microseconds
+    distances[i] = pulseIn(echoPins[i], HIGH);
+  }
+
+  Serial.print(distances[0]);
+  Serial.print(" ");
+  Serial.print(distances[1]);
+  // remove the following lines to make space for motors
+  Serial.print(" ");
+  Serial.println(distances[2]);
 }
